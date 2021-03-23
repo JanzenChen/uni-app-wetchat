c0ad62c9ce7edb11cac103511d8bcec3115ac2bc
Janzen <450415254@qq.com>
2021-03-23 11:01:38 +0800

    update

---
+++ components/general-ui/wx-button.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="main-bg-color rounded mx-2 px-2 py-1" @click="$emit()">
+		<text class="text-white font-samll">{{name}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			name: {
+				type: String,
+				default: ""
+			},
+		},
+	}
+</script>
+
+<style>
+
+</style>
--- unpackage/dist/dev/app-plus/app-config.js
+++ unpackage/dist/dev/app-plus/app-config.js
@@ -119,5 +119,7 @@ if(uni.restoreGlobal){
 
 
 
+
+
 /***/ })
 /******/ ]);
